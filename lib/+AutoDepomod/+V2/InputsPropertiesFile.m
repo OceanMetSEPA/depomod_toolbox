3ac6c8c38557c7ffa4ebb1ca6d83b1e76c81ce0b
Andrew Berkeley <andrew.berkeley@sepa.org.uk>
2017-03-02 16:29:54 +0000

    scale inputs function

--- lib/+AutoDepomod/+V2/InputsPropertiesFile.m
+++ lib/+AutoDepomod/+V2/InputsPropertiesFile.m
@@ -8,6 +8,21 @@ classdef InputsPropertiesFile < AutoDepomod.V2.DataPropertiesFile
         function IPF = InputsPropertiesFile(filePath)
             IPF = IPF@AutoDepomod.V2.DataPropertiesFile(filePath);  
         end
+        
+        function bool = scaleBiomass(IPF, factor)
+            existingBiomass         = str2num(IPF.FeedInputs.biomass);
+            existingStockingDensity = str2num(IPF.FeedInputs.stockingDensity);
+
+            IPF.FeedInputs.biomass         = num2str(existingBiomass * factor);
+            IPF.FeedInputs.stockingDensity = num2str(existingStockingDensity * factor);
+            IPF.data(:,1:2) = IPF.data(:,1:2).*factor;
+            IPF.data(:,4:5) = IPF.data(:,4:5).*factor;
+        end
+        
+        function bool = scaleChemical(IPF, factor)
+            IPF.data(:,3) = IPF.data(:,3).*factor;
+            IPF.data(:,6) = IPF.data(:,6).*factor;
+        end
     end
     
 end
